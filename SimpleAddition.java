25306a35a2f00148393af185d4b0a276bfd624ca
harshithakella02 <harshithakella02@gmail.com>
2024-11-22 10:20:48 +0530

    SimpleAddition

---
+++ SimpleAddition.java
@@ -0,0 +1,12 @@
+public class SimpleAddition
+ {
+    public static void main(String[] args)
+	{
+        int num1 = 5; // First number
+        int num2 = 7; // Second number
+
+        int sum = num1 + num2; // Adding the two numbers
+
+        System.out.println("The sum is: " + sum); // Display the result
+    }
+}
